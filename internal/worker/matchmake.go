84c4fed8c260e519502d027ba88f966a46776886
Luiz Felipe Limao <luizfelipe.limao@wildlifestudios.com>
2025-08-28 17:37:29 -0300

    feat: Implement matchmaking logic with optimal match sizing

--- internal/worker/matchmake.go
+++ internal/worker/matchmake.go
@@ -15,6 +15,8 @@ const (
 	MatchmakeQueue = "matchmake"
 	playerPoolKey  = "player_pool"
 	natsSubject    = "matchmake.request"
+	MinPlayers     = 2
+	MaxPlayers     = 16
 )
 
 type MatchmakeWorker struct {
@@ -31,50 +33,87 @@ func NewMatchmakeWorker(natsClient *nats.Conn, redisClient *redis.Client) *Match
 
 func (mw *MatchmakeWorker) Start() error {
 	_, err := mw.natsClient.QueueSubscribe(MatchmakeQueue, MatchmakeQueue, func(msg *nats.Msg) {
-		// Get the 100 oldest players in the pool
+		// Get all available players from the pool
 		ctx := context.Background()
-		result, err := mw.redisClient.ZRangeWithScores(ctx, playerPoolKey, 0, 99).Result()
+		result, err := mw.redisClient.ZRangeWithScores(ctx, playerPoolKey, 0, -1).Result()
 		if err != nil {
 			log.Printf("Error getting players from pool: %v", err)
 			return
 		}
 
-		if len(result) < 2 {
-			// Not enough players for a match
+		if len(result) < MinPlayers {
+			// Not enough players for any match
+			log.Printf("Not enough players in pool: %d (minimum: %d)", len(result), MinPlayers)
 			return
 		}
 
-		// Create a match with available players
-		players := make([]entities.Player, 0, len(result))
-		for _, z := range result {
-			playerID := z.Member.(string)
-			player := entities.Player{
-				ID:   playerID,
-				Ping: 0,
-			}
-			players = append(players, player)
-		}
-
-		match := entities.Match{
-			MatchID:   generateMatchID(),
-			Players:   players,
-			CreatedAt: time.Now(),
-		}
-
-		playerIDs := make([]interface{}, len(players))
-		for i, player := range players {
-			playerIDs[i] = player.ID
-		}
-
-		// Remove players from the pool
-		mw.redisClient.ZRem(ctx, playerPoolKey, playerIDs...)
-
-		log.Printf("Created match %s with %d players", match.MatchID, len(match.Players))
+		// Process players and create matches
+		mw.processPlayersAndCreateMatches(ctx, result)
 
 		msg.Ack()
 	})
 
 	return err
+}
+
+func (mw *MatchmakeWorker) processPlayersAndCreateMatches(ctx context.Context, players []redis.Z) {
+	// Convert Redis results to Player entities
+	playerEntities := make([]entities.Player, 0, len(players))
+	for _, z := range players {
+		playerID := z.Member.(string)
+		player := entities.Player{
+			ID:   playerID,
+			Ping: 0,
+		}
+		playerEntities = append(playerEntities, player)
+	}
+
+	matches := mw.createOptimalMatches(playerEntities)
+
+	playerIDs := make([]interface{}, len(playerEntities))
+	for i, player := range playerEntities {
+		playerIDs[i] = player.ID
+	}
+	mw.redisClient.ZRem(ctx, playerPoolKey, playerIDs...)
+
+	for _, match := range matches {
+		log.Printf("Created match %s with %d players", match.MatchID, len(match.Players))
+	}
+}
+
+func (mw *MatchmakeWorker) createOptimalMatches(players []entities.Player) []entities.Match {
+	var matches []entities.Match
+	remainingPlayers := players
+
+	for len(remainingPlayers) >= MinPlayers {
+		matchSize := mw.calculateOptimalMatchSize(len(remainingPlayers))
+
+		matchPlayers := remainingPlayers[:matchSize]
+		remainingPlayers = remainingPlayers[matchSize:]
+
+		match := entities.Match{
+			MatchID:   generateMatchID(),
+			Players:   matchPlayers,
+			CreatedAt: time.Now(),
+		}
+		matches = append(matches, match)
+	}
+
+	return matches
+}
+
+func (mw *MatchmakeWorker) calculateOptimalMatchSize(totalPlayers int) int {
+	if totalPlayers <= MaxPlayers {
+		return totalPlayers
+	}
+
+	if totalPlayers >= 24 {
+		return 12
+	} else if totalPlayers >= 18 {
+		return 9
+	} else {
+		return totalPlayers
+	}
 }
 
 func generateMatchID() string {
